622e8f427389004a6c8de473a4ab89ec93933f2d
Okhan97 <iapenafiel@uc.cl>
2019-12-30 19:35:47 -0300

    map to matrix and a bit more

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def random_vector(width, mode):
         rep *= 4
     return [randint(0,255) for _ in range(rep)]
 
-#500x500
 def gradient_img(height, width, R=False, G=False, B=False):
     def aux1(val):
         if R and val%3 == 0:
@@ -29,5 +28,55 @@ def gradient_img(height, width, R=False, G=False, B=False):
         return [aux1(i) for i in range(width*3)]
     return [ aux2() for _ in range(height)]
 
+#img to matrix
+def img_to_matrix(img):
+    matrix = []
+    vert_cont =-1
+    for line in img:
+        vert_cont +=1
+        line_aux = []
+        horz_cont =-1
+        for i in range(int(len(line)/3)):
+            horz_cont +=1
+            line_aux.append({
+                "R": line[i*3],
+                "G": line[i*3 +1],
+                "B": line[i*3 +2],
+                "y": vert_cont,
+                "x": horz_cont
+                })
+        matrix.append(line_aux)
+    return matrix
 
-png.from_array(gradient_img(500,500,R=True, B=True),"RGB").save("random.png")
+#matrix to img
+#avoids out of boundary values
+def matrix_to_img(matrix):
+    img = []
+    for line in matrix:
+        line_aux = []
+        for pix in line:
+            line_aux.append(min(max(pix["R"],0),255))
+            line_aux.append(min(max(pix["G"],0),255))
+            line_aux.append(min(max(pix["B"],0),255))
+        img.append(line_aux)
+    return img
+
+def xy_img(matrix):
+    new_img = []
+    for line in matrix:
+        new_line = []
+        for pix in line:
+            new_line.append({
+                "R": pix["y"]-pix["x"],
+                "G": pix["y"],
+                "B": pix["x"],
+            })
+        new_img.append(new_line)
+    return new_img
+
+
+img = gradient_img(500,500)
+matrix = img_to_matrix(img)
+new_mat = xy_img(matrix)
+img2 = matrix_to_img(new_mat)
+png.from_array(img2,"RGB").save("random.png")
